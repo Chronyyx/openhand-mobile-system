84a560f7fa78622b69ba2d68250f9dd5f8218abd
Chronyyx <Chronyyx@gmail.com>
2025-11-19 15:45:40 -0500

    Initial commit: Backend + Mobile App setup

---
+++ openhand-backend/src/main/java/com/mana/openhand_backend/OpenhandBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mana.openhand_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenhandBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenhandBackendApplication.class, args);
+	}
+
+}

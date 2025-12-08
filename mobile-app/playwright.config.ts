40d02c0980ff918ac2992d52a68d41a011c09461
samre <reyescifuentessamuel@gmail.com>
2025-12-08 13:57:45 -0500

    quick fix

--- mobile-app/playwright.config.ts
+++ mobile-app/playwright.config.ts
@@ -2,13 +2,21 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  timeout: 60000, // 60s per test
+
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
+
+  expect: {
+    timeout: 8000,
+  },
+
   use: {
     baseURL: 'http://localhost:8081',
+    headless: false,
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
   },

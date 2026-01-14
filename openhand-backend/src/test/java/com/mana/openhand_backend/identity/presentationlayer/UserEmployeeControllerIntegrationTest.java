0d5d5ff2373458b904cef76a997963a13f48dfb8
Jean-David Pallares <jeandavid.pallares@gmail.com>
2026-01-13 19:32:41 -0500

    Fix UserEmployeeControllerIntegrationTest: correct email validation in test

--- openhand-backend/src/test/java/com/mana/openhand_backend/identity/presentationlayer/UserEmployeeControllerIntegrationTest.java
+++ openhand-backend/src/test/java/com/mana/openhand_backend/identity/presentationlayer/UserEmployeeControllerIntegrationTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = true)
 @TestPropertySource(locations = "classpath:application-test.properties")
 class UserEmployeeControllerIntegrationTest {
 

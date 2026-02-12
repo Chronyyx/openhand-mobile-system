bb827013aa83d9f2b4ee1f65d0e14edec8a588c9
Jean-David Pallares <jeandavid.pallares@gmail.com>
2026-02-11 22:51:32 -0500

    Add admin donation detail controller

---
+++ openhand-backend/src/main/java/com/mana/openhand_backend/donations/presentationlayer/DonationAdminController.java
@@ -0,0 +1,28 @@
+package com.mana.openhand_backend.donations.presentationlayer;
+
+import com.mana.openhand_backend.donations.businesslayer.DonationService;
+import com.mana.openhand_backend.donations.domainclientlayer.DonationDetailResponseModel;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/admin/donations")
+@PreAuthorize("hasRole('ADMIN')")
+public class DonationAdminController {
+
+    private final DonationService donationService;
+
+    public DonationAdminController(DonationService donationService) {
+        this.donationService = donationService;
+    }
+
+    @GetMapping("/{id}")
+    public DonationDetailResponseModel getDonationDetail(@PathVariable Long id) {
+        return donationService.getDonationDetail(id);
+    }
+}

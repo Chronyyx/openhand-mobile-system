2dd15214031493477b1a4d08821eb1eed56f1653
Maximilian Coreachin <coreachinmax@gmail.com>
2026-02-15 18:03:56 -0500

    Update tests: Replace `@MockBean` with `@MockitoBean` in `DonationManagementControllerTest`

--- openhand-backend/src/test/java/com/mana/openhand_backend/donations/presentationlayer/DonationManagementControllerTest.java
+++ openhand-backend/src/test/java/com/mana/openhand_backend/donations/presentationlayer/DonationManagementControllerTest.java
@@ -14,9 +14,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -37,10 +37,10 @@ class DonationManagementControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private DonationService donationService;
 
-    @MockBean
+    @MockitoBean
     private UserRepository userRepository;
 
     @Test
